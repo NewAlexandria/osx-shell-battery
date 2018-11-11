a9725e719b22a73057be227992554ee1b3622c0d
Zachary Jones <zak@newalexandria.org>
2018-11-10 22:57:13 -0500

    add batter time function

--- functions_battery.sh
+++ functions_battery.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/sh
+# Tested working on OSX Mojave
 
 # I need to cut field-1 for this to trim evenly
+# print the battery state with colors based on level
 function __batt_state() {
   bstate=$(pmset -g batt | awk '/charging|discharging|charged/ {print $4}' | cut -f1 -d';')
   yellow="$(printf '\033[33m')"
@@ -28,5 +30,12 @@ function __batt_state() {
 
 function __batt_pct() {
   pmset -g batt | egrep '([0-9]+\%).*' -o | cut -f1 -d';'
+}
+
+# now, as a function, we can easily handle a conditional
+function __batt_time() {
+  btime=$(pmset -g batt | grep -Eo '([0-9][0-9]|[0-9]):[0-5][0-9]')
+  btime=${btime:2}
+  if [[ "$btime" == "0:00" ]]; then echo ''; else echo " [$btime]"; fi
 }
 
